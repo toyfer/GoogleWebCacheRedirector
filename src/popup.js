8a5f3d9320d96e79b92601eeac99697ff29fe8fc
toyfer <nicotooy573@gmail.com>
2022-12-18 22:57:49 +0900

    初期コミット

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,10 @@
-const toggle = document.getElementById("toggle");
-
-// Get current extension state
-chrome.storage.sync.get("enabled", function(result) {
-  toggle.checked = result.enabled;
-});
-
-// Update extension state when toggle is clicked
-toggle.addEventListener("click", function() {
-  chrome.storage.sync.set({ enabled: toggle.checked });
-});
+// Set initial state of toggle
+chrome.storage.sync.get(['enabled'], function(result) {
+    document.querySelector('.toggle').checked = result.enabled;
+  });
+  
+  // Update state of toggle
+  document.querySelector('.toggle').addEventListener('change', function(event) {
+    chrome.storage.sync.set({enabled: event.target.checked});
+  });
+  
