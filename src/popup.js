fa1fbbe1cd699f72cfd14d9c1aa4f6423dca280d
toyfer <nicotooy573@gmail.com>
2022-12-19 11:30:08 +0900

    初期コミット

--- src/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var checkbox = document.getElementById("enabled");
-  chrome.storage.sync.get("enabled", function (items) {
-    checkbox.checked = items.enabled;
-  });
-  checkbox.addEventListener("change", function () {
-    chrome.storage.sync.set({ enabled: checkbox.checked });
-  });
-});
