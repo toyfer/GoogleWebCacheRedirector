5cd025924917735e1c993f10ae41596e62a77c37
toyfer <nicotooy573@gmail.com>
2022-12-19 10:19:01 +0900

    初期コミット

--- src/background.js
+++ src/background.js
@@ -1,12 +1,12 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) { // Webリクエストが送信される前に実行されるコールバック関数
-  var url = new URL(details.url); // URLオブジェクトを生成
-  if (url.searchParams.has("q") && url.searchParams.has("oq")) { // Google検索リンクを識別するためのURLパラメータを確認
-  // This is a Google search result link
-  var cacheUrl = "https://webcache.googleusercontent.com/search?q=cache:" + url.href; // キャッシュURLを生成
-  return {redirectUrl: cacheUrl}; // 新しいURLにリダイレクト
-  }
+  function (details) { // Webリクエストが送信される前に実行されるコールバック関数
+    var url = new URL(details.url); // URLオブジェクトを生成
+    if (url.searchParams.has("q") && url.searchParams.has("oq")) { // Google検索リンクを識別するためのURLパラメータを確認
+      // This is a Google search result link
+      var cacheUrl = "https://webcache.googleusercontent.com/search?q=cache:" + url.href; // キャッシュURLを生成
+      return { redirectUrl: cacheUrl }; // 新しいURLにリダイレクト
+    }
   },
-  {urls: ["*://www.google.com/search/*"]}, // Google検索リンクをクリックする前にリクエストを確認するためのフィルター
+  { urls: ["*://www.google.com/search/*"] }, // Google検索リンクをクリックする前にリクエストを確認するためのフィルター
   ["blocking"] // リクエストを完全にブロックするためのフラグ
-  );
+);
--- src/options.js
+++ src/options.js
@@ -1,10 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var checkbox = document.getElementById("enabled");
-    chrome.storage.sync.get("enabled", function(items) {
-      checkbox.checked = items.enabled;
-    });
-    checkbox.addEventListener("change", function() {
-      chrome.storage.sync.set({enabled: checkbox.checked});
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var checkbox = document.getElementById("enabled");
+  chrome.storage.sync.get("enabled", function (items) {
+    checkbox.checked = items.enabled;
   });
-  
+  checkbox.addEventListener("change", function () {
+    chrome.storage.sync.set({ enabled: checkbox.checked });
+  });
+});
