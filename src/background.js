ab4f8f625b2c68eee1000aed363138d44ac021b3
toyfer <nicotooy573@gmail.com>
2022-12-19 10:03:14 +0900

    初期コミット

--- src/background.js
+++ src/background.js
@@ -1,13 +1,12 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      var url = new URL(details.url);
-      if (url.searchParams.has("q") && url.searchParams.has("oq")) {
-        // This is a Google search result link
-        var cacheUrl = "https://webcache.googleusercontent.com/search?q=cache:" + url.href;
-        return {redirectUrl: cacheUrl};
-      }
-    },
-    {urls: ["*://www.google.com/search*"]},
-    ["blocking"]
+  function(details) { // Webリクエストが送信される前に実行されるコールバック関数
+  var url = new URL(details.url); // URLオブジェクトを生成
+  if (url.searchParams.has("q") && url.searchParams.has("oq")) { // Google検索リンクを識別するためのURLパラメータを確認
+  // This is a Google search result link
+  var cacheUrl = "https://webcache.googleusercontent.com/search?q=cache:" + url.href; // キャッシュURLを生成
+  return {redirectUrl: cacheUrl}; // 新しいURLにリダイレクト
+  }
+  },
+  {urls: ["://www.google.com/search"]}, // Google検索リンクをクリックする前にリクエストを確認するためのフィルター
+  ["blocking"] // リクエストを完全にブロックするためのフラグ
   );
-  
