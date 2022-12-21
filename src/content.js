83cd90571edc154d61baa0ff7b662e61a0b6d32f
toyfer <nicotooy573@gmail.com>
2022-12-21 13:49:18 +0900

    初期コミット

--- src/content.js
+++ src/content.js
@@ -1,22 +1,23 @@
 // 拡張機能のON/OFF状態を管理する
-let enabled = true;
+let extesionStatus = null;
 
-chrome.storage.sync.get(['enabled'], function (result) {
-    enabled = result.enabled;
+chrome.storage.sync.get(['extesionStatus'], function (result) {
+    extesionStatus = result.extesionStatus;
+
+    // Googleの遷移先のページがGoogle以外のサイトだった場合
+    if (extesionStatus) {
+        let nowurl = location.href;
+        if (!nowurl.startsWith('https://www.google.com')) {
+            window.location.replace("https://www.google.com/search?q=" + "cache:" + nowurl);
+        }
+    }
 });
 
+// popup.jsからの通知を受け取り反映する
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message === 'toggle') {
-        enabled = !enabled;
-        chrome.storage.sync.set({ enabled: enabled });
-        chrome.runtime.sendMessage({ message: 'status', enabled: enabled });
+        extesionStatus = !extesionStatus;
+        chrome.storage.sync.set({ extesionStatus: extesionStatus });
+        chrome.runtime.sendMessage({ message: 'status', extesionStatus: extesionStatus });
     }
 });
-
-// Googleの遷移先のページがGoogle以外のサイトだった場合
-if (enabled) {
-    let nowurl = location.href;
-    if (!nowurl.startsWith('https://www.google.com')) {
-        window.location.replace("https://www.google.com/search?q=" + "cache:" + nowurl);
-    }
-}
--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,14 @@
+// 拡張機能の現在のステータスを記録する
+chrome.storage.sync.get(['extesionStatus'], function (result) {
+    extesionStatus = result.extesionStatus;
+    if (extesionStatus) {
+        document.getElementById('toggle-button').innerText = 'ON';
+        document.getElementById('status').innerText = 'Status: ON';
+    } else {
+        document.getElementById('toggle-button').innerText = 'OFF';
+        document.getElementById('status').innerText = 'Status: OFF';
+    }
+});
 // 拡張機能のON/OFFを切り替える
 document.getElementById('toggle-button').addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -9,7 +20,7 @@ document.getElementById('toggle-button').addEventListener('click', function () {
 // content.jsからの通知を受け取り、ON/OFF状態を更新する
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message === 'status') {
-        if (request.enabled) {
+        if (request.extesionStatus) {
             document.getElementById('toggle-button').innerText = 'ON';
             document.getElementById('status').innerText = 'Status: ON';
         } else {
